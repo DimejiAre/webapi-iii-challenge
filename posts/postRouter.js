1d3f3b27d67e83d2db2e55a490563a1d4b8c6488
DimejiAre <olardi2@gmail.com>
2019-10-30 16:12:36 +0100

    add endpoint for getting posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,18 @@
-const express = 'express';
+const express = require('express');
+const postDb = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    postDb.get()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The post information could not be retrieved. " + err
+            })
+        })
 });
 
 router.get('/:id', (req, res) => {
@@ -21,7 +30,23 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    const id = req.params.id;
+    postDb.getById(id)
+        .then(data => {
+            if(data){
+                req.post = data
+            }
+            else {
+                res.status(400).json({
+                    message: "invalid post id"
+                })
+            }
+        })
+        .catch( err => {
+            res.status(500).json({
+                error: "Post information could not be retrieved " + err
+            })
+        })
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const users = require('./users/userRouter');
+const posts = require('./posts/postRouter');
 
 const server = express();
 
 server.use(express.json());
 server.use(logger);
 server.use('/api/users', users)
+server.use('/api/posts', posts)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
