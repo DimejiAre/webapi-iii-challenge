ff95b9a2868c943f74c672dc245b8ce3e9a19ac5
DimejiAre <olardi2@gmail.com>
2019-10-30 16:31:29 +0100

    add endpoints to get by id, update and delete a post

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -15,16 +15,41 @@ router.get('/', (req, res) => {
         })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', [validatePostId], (req, res) => {
+    res.json(req.post)
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', [validatePostId], (req, res) => {
+    const id = req.params.id;
+    
+    postDb.remove(id)
+        .then(data => {
+            res.status(200).json({
+                message: `${data} post deleted`
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The post could not be removed " + err
+            })
+        })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', [validatePostId], (req, res) => {
+    const id = req.params.id;
+    const user = req.body;
 
+    postDb.update(id, user)
+        .then(() => {
+            postDb.getById(id).then(updatedPost => {
+                res.status(200).json(updatedPost)
+            })
+        })
+        .catch(() => {
+                res.status(500).json({
+                    error: "There was an error while saving the post to the database"
+                })
+            })
 });
 
 // custom middleware
@@ -35,6 +60,7 @@ function validatePostId(req, res, next) {
         .then(data => {
             if(data){
                 req.post = data
+                next()
             }
             else {
                 res.status(400).json({
